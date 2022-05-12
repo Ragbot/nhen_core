3835ab55ed97d3a44d2ee8a28457acce63860f40
Ragbot <shaletgasmer@gmail.com>
2022-05-12 20:20:56 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <curl/curl.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#define NHEN "https://nhentai.net"
+
+struct memory
+{
+	char* response;
+	size_t size;
+};
+
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct memory *mem = (struct memory *)userp;
+ 
+  char *ptr = realloc(mem->response, mem->size + realsize + 1);
+  if(!ptr) {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+ 
+  mem->response = ptr;
+  memcpy(&(mem->response[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->response[mem->size] = 0;
+ 
+  return realsize;
+}
+
+int main()
+{
+	struct memory mem = {0};
+
+	CURL* curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+
+	curl_easy_setopt(curl, CURLOPT_URL, NHEN);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&mem);
+
+	res = curl_easy_perform(curl);
+
+	if(res != CURLE_OK) {
+		printf("Failed to execute curle\n");
+		return EXIT_FAILURE;
+	}
+
+	printf(mem.response);
+
+}
+
